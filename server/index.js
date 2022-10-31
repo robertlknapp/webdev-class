4b1bad50f3c5a68d59c3e460e8879a71faa1059c
robertlknapp <robertlknapp@outlook.com>
2022-10-31 15:16:44 -0400

    server-side setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const productsController = require('./controllers/products');
+
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Happy Sweet New Year');
-})
+//responds to get requests
+app
+  .get('/', (req, res) => {
+
+    res.status(200).send('Happy Sweet New Year');
+
+  })
+  .use('/api/v1/products', productsController)
 
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
